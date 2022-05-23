d35202db23268cbad463b5c0e74afd78c7764aab
IfunanyaWilliam <williamifunanya@gmail.com>
2022-05-23 14:11:05 +0100

    Change dependency injection servies from AddSingleton to AddScoped

--- OFood/Program.cs
+++ OFood/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //Add service for Restaurant
-builder.Services.AddSingleton<IRestaurantData, SqlRestaurantData>();
+builder.Services.AddScoped<IRestaurantData, SqlRestaurantData>();
 
 var app = builder.Build();
 
